512674ebd979c6f2867c2a8450e13ca19680446e
kifu <kifu.100@gmail.com>
2016-12-13 19:21:13 +0100

    hh

--- Pacman5/ghost.cpp
+++ Pacman5/ghost.cpp
@@ -19,39 +19,53 @@ Ghost::Ghost(int x, int y, char symbol)
 }
 
 void Ghost::setGhostPosition(Map *map, Player *player) {
+	//zapisujemy sobie lokalnie pozycje duszka
 	int x = this->x;
 	int y = this->y;
+	// ustawiamy minimalny indeks i minimalna odleglosc na nieosiagalne wartosci(o tym pozniej)
 	int minIndex = -1;
 	int minValue = 1000;
+	// tworzymy tablice z potencjalnymi punktami do odwiedzenia dla duszka, oraz domyslna(duza) odlegloscia duszka od gracza
 	int pointsToCheck[4][3] = {
 		{ this->x + 1, this->y, 1000 },
 		{ this->x - 1, this->y, 1000 },
 		{ this->x, this->y + 1, 1000 },
 		{ this->x, this->y - 1, 1000 } };
 
+
 	for (int i = 0; i < 4; i++) {
+		// dla kazdego punktu - sprawdzamy, czy jest on osiagalny(czy mozna tam stanac), oraz czy jest poza jaskinia
 		if (map->isPointAccessible(pointsToCheck[i][0], pointsToCheck[i][1], this) && this->outsideLair) {
+			// jezeli gracz ma bonus - duszki powinny uciekac
 			if (player->hasBonus) {
+				// obliczamy sobie sin(180*) i cos(180*) - potrzebny do obrotu
 				float originSin = sin(180 * M_PI / 180);
 				float originCos = cos(180 * M_PI / 180);
+				// ustawiamy punkt, wokol ktorego dokonamy obrotu
 				int originX = 15;
 				int originY = 14;
+				// obliczamy(w przyblizeniu) punkt przeciwlegly do punktu gracza- duszki beda tam uciekac
 				int oppositePlayerX = floor((player->x - originX) * originCos - (player->y - originY) * originSin + originX);
 				int oppositePlayerY = floor((player->x - originX) * originSin + (player->y - originY) * originCos + originY);
 
-				pointsToCheck[i][2] = -map->euclideanDistance(pointsToCheck[i][0], pointsToCheck[i][1], oppositePlayerX, oppositePlayerY);
+				// i sprawdzamy, ktory jest "najdalej"
+				pointsToCheck[i][2] = map->euclideanDistance(pointsToCheck[i][0], pointsToCheck[i][1], oppositePlayerX, oppositePlayerY);
 			}
 			else {
+				// w przeciwnym wypadku, obliczamy dla kazdego dostepnego punktu - jak daleko jest gracz
 				pointsToCheck[i][2] = map->euclideanDistance(pointsToCheck[i][0], pointsToCheck[i][1], player->x, player->y);
 			}
 		}
-		else if (this->outsideLair) {
+		// jezeli duszek nie jest poza jaskinia
+		else if (!this->outsideLair) {
+			// szukamy najlepszych punktow na ktore moze sie przemiescic, zeby trafic na (15,11)
 			int xToGo = 15;
 			int yToGo = 11;
 			pointsToCheck[i][2] = map->euclideanDistance(pointsToCheck[i][0], pointsToCheck[i][1], xToGo, yToGo);
 		}
 	}
 
+	// wyszukujemy najmniejsza wartosc odleglosci z punktow do sprawdzenia, oraz indeks punktu z najmniejsza odlegloscia
 	for (int i = 0; i < 4; i++) {
 		if (pointsToCheck[i][2] < minValue) {
 			minValue = pointsToCheck[i][2];
@@ -59,6 +73,8 @@ void Ghost::setGhostPosition(Map *map, Player *player) {
 		}
 	}
 
+	// dodajemy troche losowosci - kiedy indeks >=0 - wtedy istnieje punkt o minimalnej odleglosci i jest on pod indeksem minIndex, dodatkowo losujemy wartosc
+	// od 0-9, kiedy wartosc jest > 4, idziemy do tego punktu - w przeciwnym wypadku wybieramy losowo pozycje do przemieszczenia
 	if ((minIndex >= 0) && (rand() % 10) > 4) {
 		x = pointsToCheck[minIndex][0];
 		y = pointsToCheck[minIndex][1];
@@ -85,7 +101,6 @@ void Ghost::setGhostPosition(Map *map, Player *player) {
 		this->y = y;
 	}
 }
-
 
 
 void Ghost::movedFromLair()
